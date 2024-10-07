e682bfde7700958a3ce9aabb45b9d513e3910417
Shrinav Loka <lokashrinav@gmail.com>
2024-10-06 22:16:30 -0400

    done

---
+++ src/module/code.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const axesHelper = new THREE.AxesHelper(5);
+
+camera.position.z = 5
+camera.position.x = 6
+
+scene.add(axesHelper);
+
+renderer.render(scene, camera);
+
